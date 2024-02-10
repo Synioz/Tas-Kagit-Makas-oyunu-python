cbe14609ddaa32c3d4a6378c35abdf1f57583489
Synioz <128211036+Synioz@users.noreply.github.com>
2024-02-10 15:52:16 +0300

    Add files via upload

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -19,11 +19,12 @@ userselect = {
 usertrue = ['evet', 'yes', 'olur']
 userfalse = ['yok', 'hayır', 'no', 'hayir', 'olmaz', 'çık', 'exit', 'cik']
 
-def winandlose(userinput, inputn):
+def winandlose(userselection, inputn):
     pcselection = pcrandom()
-    if (pcselection == 'Makas' and userinput == 1) or (pcselection == 'Taş' and userinput == 2) or (pcselection == 'Kağıt' and userinput == 3):
+    pcput = r.randint(1,3)
+    if (pcput == 3 and userselection == 1) or (pcput == 1 and userselection == 2) or (pcput == 2 and userselection == 3):
         return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nTebrikler sen kazandın!'
-    elif (pcselection == 'Taş' and userinput == 3) or (pcselection == 'Kağıt' and userinput == 1) or (pcselection == 'Makas' and userinput == 2):
+    elif (pcput == 1 and userselection == 3) or (pcput == 2 and userselection == 1) or (pcput == 3 and userselection == 2):
         return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nSen kaybettin.'
     else:
         return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nBerabere.'
@@ -35,17 +36,17 @@ def hello():
     while True:
         if firstput in usertrue:
             print('Bunu duyduğuma sevindim!')
-            inputn = input('Taş, Kağıt, Makas oyununa hoş geldiniz\nLütfen bir seçim yapın(Taş, Kağıt, Makas vb.)\nSiz: ')
+            inputn = input('Taş, Kağıt, Makas oyununa hoş geldiniz,\nLütfen bir seçim yapın.(Taş, Kağıt, Makas vb.)\nSiz: ')
             userinput = inputn.lower()
             userselection = userselect.get(userinput)
 
             if userselection:
-                winandloser = winandlose(userinput, inputn)
+                winandloser = winandlose(userselection, inputn)
                 print(winandloser)
                 break
 
         elif firstput in userfalse:
-            print('Bunu duyduğuma üzüldüm program kapatılıyor...')
+            print('Bunu duyduğuma üzüldüm, program kapatılıyor...')
             break
         else:
             break
