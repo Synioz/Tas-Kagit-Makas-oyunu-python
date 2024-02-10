1741a8f29803f4251c40c339e2538a441c28eb2e
Synioz <128211036+Synioz@users.noreply.github.com>
2024-02-10 15:46:27 +0300

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,58 @@
+import random as r
+
+randompc = {
+    1: 'Taş',
+    2: 'Kağıt',
+    3: 'Makas'
+}
+userselect = {
+    'rock': 1,
+    'paper': 2,
+    'scissos': 3,
+    'taş': 1,
+    'kağıt': 2,
+    'makas': 3,
+    'tas': 1,
+    'kagit': 2
+
+}
+usertrue = ['evet', 'yes', 'olur']
+userfalse = ['yok', 'hayır', 'no', 'hayir', 'olmaz', 'çık', 'exit', 'cik']
+
+def winandlose(userinput, inputn):
+    pcselection = pcrandom()
+    if (pcselection == 'Makas' and userinput == 1) or (pcselection == 'Taş' and userinput == 2) or (pcselection == 'Kağıt' and userinput == 3):
+        return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nTebrikler sen kazandın!'
+    elif (pcselection == 'Taş' and userinput == 3) or (pcselection == 'Kağıt' and userinput == 1) or (pcselection == 'Makas' and userinput == 2):
+        return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nSen kaybettin.'
+    else:
+        return f'Sen {inputn} dedin. Bilgisayar {pcselection} dedi.\nBerabere.'
+
+def hello():
+    n = input('Taş, Kağıt, Makas oynamak ister misiniz?\nSiz: ')
+    
+    firstput = n.lower()
+    while True:
+        if firstput in usertrue:
+            print('Bunu duyduğuma sevindim!')
+            inputn = input('Taş, Kağıt, Makas oyununa hoş geldiniz\nLütfen bir seçim yapın(Taş, Kağıt, Makas vb.)\nSiz: ')
+            userinput = inputn.lower()
+            userselection = userselect.get(userinput)
+
+            if userselection:
+                winandloser = winandlose(userinput, inputn)
+                print(winandloser)
+                break
+
+        elif firstput in userfalse:
+            print('Bunu duyduğuma üzüldüm program kapatılıyor...')
+            break
+        else:
+            break
+
+def pcrandom():
+    random = r.randint(1,3)
+    return randompc.get(random)
+
+
+hello()
